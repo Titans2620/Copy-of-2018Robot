f611249f1d0e1bb03dae7217a8750edaa44f1f71
Titans2620 <starobotics2620@gmail.com>
2018-02-06 19:01:45 -0500

    Auton Work

--- src/org/usfirst/frc/team2620/robot/Robot.java
+++ src/org/usfirst/frc/team2620/robot/Robot.java
@@ -30,14 +30,14 @@ import edu.wpi.first.wpilibj.CameraServer;
 
 public class Robot extends TimedRobot {
 	
-	WPI_TalonSRX driveRight = new WPI_TalonSRX(1);
-	WPI_TalonSRX driveLeft = new WPI_TalonSRX(5);
+	WPI_TalonSRX driveRight = new WPI_TalonSRX(5);
+	WPI_TalonSRX driveLeft = new WPI_TalonSRX(1);
 
 	WPI_TalonSRX stage2Right = new WPI_TalonSRX(2);
-	WPI_TalonSRX stage2Left= new WPI_TalonSRX(6);
-	WPI_TalonSRX carriage = new WPI_TalonSRX(7);
-	WPI_TalonSRX pickupRight = new WPI_TalonSRX(8);
-	WPI_TalonSRX pickupLeft = new WPI_TalonSRX(9);
+	WPI_TalonSRX stage2Left= new WPI_TalonSRX(2);
+	WPI_TalonSRX carriage = new WPI_TalonSRX(3);
+	WPI_TalonSRX pickupRight = new WPI_TalonSRX(6);
+	WPI_TalonSRX pickupLeft = new WPI_TalonSRX(7);
 	
 	Servo climbLock = new Servo(0);
 	
@@ -45,7 +45,7 @@ public class Robot extends TimedRobot {
 	Encoder driveRightEncoder = new Encoder(2, 3, false, Encoder.EncodingType.k4X);	
 	Encoder driveCarriageEncoder = new Encoder(4,5, false, Encoder.EncodingType.k4X);
 	
-	Ultrasonic frontDistance = new Ultrasonic(9, 10);
+	Ultrasonic frontDistance; // = new Ultrasonic(9, 10);
 	ADXRS450_Gyro gyro;
 	
 	DigitalInput stage2TopStop = new DigitalInput(6);
@@ -63,11 +63,12 @@ public class Robot extends TimedRobot {
 
 	// Auton Vars
 	private Timer autonTimer;
-	private SendableChooser autonChooser;
+	private SendableChooser<Integer> autonChooser;
 	private int autonMode;
 	private String autonGameData;
-	private double autonDriveSpeed = 0.4; // In % Power
+	private double autonDriveSpeed = 0.2; // In % Power
 	private double autonDriveTime = 10; // Game Time In Seconds (NOT RUNNING TIME)
+	private int autonStraightOnlyDistance = 5 * 12;
 	private boolean auton_atTarget = false;
 	private boolean auton_centered = false;
 	private boolean auton_atHeight = false;
@@ -79,6 +80,7 @@ public class Robot extends TimedRobot {
 		// TODO: ROBOT NEEDS GYRO AND ULTRASONIC AND CAMERA AND LIMIT SWITCHES
 		
 		gyro = new ADXRS450_Gyro();
+		gyro.calibrate();
 
 		// Setup Encoders, https://wpilib.screenstepslive.com/s/currentCS/m/java/l/599717-encoders-measuring-rotation-of-a-wheel-or-other-shaft
 		driveLeftEncoder.setMaxPeriod(.1);
@@ -94,19 +96,19 @@ public class Robot extends TimedRobot {
 		driveRightEncoder.setSamplesToAverage(7);
 
 		// Setup Auton Chooseable
-		autonChooser = new SendableChooser();
+		autonChooser = new SendableChooser<Integer>();
 		autonChooser.addDefault("ROBOT STRAIGHT ONLY", 1);
 		autonChooser.addObject("TARGET SWITCH - ROBOT is LEFT", 2);
 		autonChooser.addObject("TARGET SWITCH - ROBOT is RIGHT", 3);
 		autonChooser.addObject("TARGET SCALE -- ROBOT is LEFT", 4);
 		autonChooser.addObject("TARGET SCALE -- ROBOT is RIGHT", 5);
-
+		SmartDashboard.putData("Auton Mode", autonChooser);
 	}
 
 	public void drive(double left, double right) {
 		// Powers the drives motors, guarantees positive is forward for both left & right
-		driveRight.set(ControlMode.PercentOutput, right * -1.0);
-		driveLeft.set(ControlMode.PercentOutput, left);
+		driveRight.set(ControlMode.PercentOutput, right);
+		driveLeft.set(ControlMode.PercentOutput, left * -1.0);
 	}
 
 	public void pickup(double speed) {
@@ -116,10 +118,16 @@ public class Robot extends TimedRobot {
 	}
 
 	public void autonomousInit() {
+		gyro.reset();
+		
+		auton_atTarget = false;
+		auton_centered = false;
+		auton_atHeight = false;
+
 		autonGameData = DriverStation.getInstance().getGameSpecificMessage();
 		autonMode = (int) autonChooser.getSelected();
 		autonTimer = new Timer();
-		frontDistance.setAutomaticMode(true);
+		// frontDistance.setAutomaticMode(true);
 		driveLeftEncoder.reset();
 		driveRightEncoder.reset();
 	}
@@ -157,113 +165,135 @@ public class Robot extends TimedRobot {
 		boolean shouldPlaceLeft = (autonGameData.charAt(0) == 'L' && autonMode == 2);
 		boolean shouldPlaceRight = (autonGameData.charAt(0) == 'R' && autonMode == 3);
 
-		if(autonTimer.getMatchTime() < 1.0) {
-			drive(autonDriveSpeed, autonDriveSpeed);
+		if(!shouldPlaceLeft && !shouldPlaceRight) {
+			// If our switch isnt on robots side, just drive past black line
+			auton_driveStraightDistance(autonStraightOnlyDistance);
 		} else {
-
-			if(shouldPlaceLeft || shouldPlaceRight) 
-			{
-				if(!auton_centered) {
-					// Turn left or right to center the robot on the target
-
-					// Resolution is 640x480 I think?
-
-					NetworkTableInstance instance = NetworkTableInstance.getDefault();
-		  			NetworkTable rootTable = instance.getTable("GRIP");
-					double centerX = rootTable.getSubTable("contours").getEntry("centerX").getDouble(640.0);
-					double centerPercent = centerX / 640.0;
-					double centerErrorPercent = 10.0;
-
-					if(centerPercent > (50.0 - centerErrorPercent)) {
-						auton_sideLastSeen = "R";
-					} else if(centerPercent < (50.0 - centerErrorPercent)) {
-						auton_sideLastSeen = "L";
+			if(autonTimer.getMatchTime() < 1.0) {
+				drive(autonDriveSpeed, autonDriveSpeed);
+			} else {
+	
+				if(shouldPlaceLeft || shouldPlaceRight) 
+				{
+					if(!auton_centered) {
+						// Turn left or right to center the robot on the target
+	
+						// Resolution is 640x480 I think?
+	
+						NetworkTableInstance instance = NetworkTableInstance.getDefault();
+			  			NetworkTable rootTable = instance.getTable("GRIP");
+						double centerX = rootTable.getSubTable("contours").getEntry("centerX").getDouble(640.0);
+						double centerPercent = centerX / 640.0;
+						double centerErrorPercent = 10.0;
+	
+						if(centerPercent > (50.0 - centerErrorPercent)) {
+							auton_sideLastSeen = "R";
+						} else if(centerPercent < (50.0 - centerErrorPercent)) {
+							auton_sideLastSeen = "L";
+						} else {
+							auton_sideLastSeen = "C";
+							auton_centered = true;
+						}
+	
+						double leftDirection = 0.0;
+						double rightDirection = 0.0;
+	
+						if(auton_sideLastSeen == "R") {
+							// Turn robot left to get to the center
+							leftDirection = -1.0;
+							rightDirection = 1.0;
+						} else if (auton_sideLastSeen == "L") {
+							// Turn robot right to get to the center
+							leftDirection = 1.0;
+							rightDirection = -1.0;
+						}
+	
+						drive(leftDirection * 0.2, rightDirection * -0.2);
+					} else if (!auton_atTarget) {
+						// Drive forward until we are very close to the target
+						if (frontDistance.getRangeInches() < 6) { 
+							// Within 6 inches of something
+							auton_atTarget = true;
+							drive(0.0, 0.0);
+						} else {
+							auton_driveStraightOnly();
+						}
 					} else {
-						auton_sideLastSeen = "C";
-						auton_centered = true;
-					}
-
-					double leftDirection = 0.0;
-					double rightDirection = 0.0;
-
-					if(auton_sideLastSeen == "R") {
-						// Turn robot left to get to the center
-						leftDirection = -1.0;
-						rightDirection = 1.0;
-					} else if (auton_sideLastSeen == "L") {
-						// Turn robot right to get to the center
-						leftDirection = 1.0;
-						rightDirection = -1.0;
-					}
-
-					drive(leftDirection * 0.2, rightDirection * -0.2);
-				} else if (!auton_atTarget) {
-					// Drive forward until we are very close to the target
-					if (frontDistance.getRangeInches() < 6) { 
-						// Within 6 inches of something
-						auton_atTarget = true;
-						drive(0.0, 0.0);
-					} else {
-						auton_driveStraightOnly();
+						// Spit out the crate
+						pickup(pickupSpeed * -1);
 					}
 				} else {
-					// Spit out the crate
-					pickup(pickupSpeed * -1);
+					auton_driveStraightOnly();
 				}
-			} else {
-				auton_driveStraightOnly();
 			}
 		}
 	}
 
 	private void auton_placeInScale()
-	{
-		boolean shouldPlaceLeft = (autonGameData.charAt(0) == 'L' && autonMode == 4);
-		boolean shouldPlaceRight = (autonGameData.charAt(0) == 'R' && autonMode == 5);
-
-		if(!auton_centered) {
-			// Drive forward directly next too scale
-			auton_centered = auton_driveStraightDistance(10 * 12); // Drives forward 10 feet
-		} else if (!auton_atHeight) {
-			// Move to required height to place on scale
-
-			if(!stage2TopStop.get()) {
-				// Move Stage 2 To Top
-				stage2Right.set(stage2Speed * -1);
-				stage2Left.set(stage2Speed * -1);
-			} else {
-				stage2Right.set(0.0);
-				stage2Left.set(0.0);
-			}
-
-			if(!carriageTopStop.get()) {
-				// Move Carriage To Top
-				carriage.set(carriageSpeed * 1);
-			} else {
-				carriage.set(0.0);
-			}
-
-			if(carriageTopStop.get() && stage2TopStop.get()) {
-				auton_atHeight = true;
-				gyro.reset();
-			}
-		} else if(!auton_atTarget) {
-			// Turn 90 degrees to face target
-
-			if(shouldPlaceLeft) {
-				// Turn Right
-				if(gyro.getAngle() > 90) {
-					auton_atTarget = true;
-				}
-			} else {
-				if(gyro.getAngle() < 90) {
-					auton_atTarget = true;
-				}
-			}
-
+	{		
+		boolean shouldPlaceLeft = (autonGameData.charAt(1) == 'R' && autonMode == 4);
+		boolean shouldPlaceRight = (autonGameData.charAt(1) == 'L' && autonMode == 5);
+		
+		if(!shouldPlaceLeft && !shouldPlaceRight) {
+			// If our scale isnt on robots side, just drive past black line
+			auton_driveStraightDistance(autonStraightOnlyDistance);
 		} else {
-			// Spit out crate
-			pickup(pickupSpeed * -1);
+			// Scale is scoreable from our position
+
+			// TODO: REMOVE THE TWO HARD BOOL SETS BELOW
+			auton_centered = true; // Skip this step
+			auton_atHeight = true; // Skip this step
+			
+			if(!auton_centered) {
+				// Drive forward directly next too scale
+				auton_centered = auton_driveStraightDistance(10 * 12); // Drives forward 10 feet
+			} else if (!auton_atHeight) {
+				// Move to required height to place on scale
+	
+				if(!stage2TopStop.get()) {
+					// Move Stage 2 To Top
+					stage2Right.set(stage2Speed * -1);
+					stage2Left.set(stage2Speed * -1);
+				} else {
+					stage2Right.set(0.0);
+					stage2Left.set(0.0);
+				}
+	
+				if(!carriageTopStop.get()) {
+					// Move Carriage To Top
+					carriage.set(carriageSpeed * 1);
+				} else {
+					carriage.set(0.0);
+				}
+	
+				if(carriageTopStop.get() && stage2TopStop.get()) {
+					auton_atHeight = true;
+					gyro.reset();
+				}
+			} else if(!auton_atTarget) {
+				// Turn 90 degrees to face target
+				
+				System.out.println(gyro.getAngle());
+	
+				if(shouldPlaceLeft) {
+					// Turn Right 90 degrees
+					drive(autonDriveSpeed, autonDriveSpeed * -1.0);
+					if(gyro.getAngle() > 88) {
+						drive(0.0, 0.0);
+						auton_atTarget = true;
+					}
+				} else if(shouldPlaceRight) {
+					// Turn Left 90 degrees
+					drive(autonDriveSpeed * -1.0, autonDriveSpeed);
+					if(gyro.getAngle() < -88) {
+						drive(0.0, 0.0);
+						auton_atTarget = true;
+					}
+				}
+			} else {
+				// Spit out crate
+				pickup(pickupSpeed * -1);
+			}
 		}
 	}
 
@@ -283,7 +313,7 @@ public class Robot extends TimedRobot {
 				auton_placeInScale();
 				break;
 			default:
-				auton_driveStraightDistance(5 * 12); // Drive straight 5 feet
+				auton_driveStraightDistance(autonStraightOnlyDistance);
 				break;
 		}
 	}
@@ -295,8 +325,6 @@ public class Robot extends TimedRobot {
 	public void  teleopPeriodic() 
 	{
 		drive(left.getY(), right.getY());
-
-		System.out.println('Gyro:', gyro.getAngle());
 		
 		boolean lTrigger = left.getRawButton(1);
 		boolean lFaceB = left.getRawButton(2);
